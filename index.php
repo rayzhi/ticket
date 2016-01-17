c566a65ecd5b1226d5cd86c8e5629ce709b90907
dasheng523 <dasheng523@163.com>
2016-01-17 15:30:53 +0800

    修复index.php的地址

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',true);
 
-defined('__BASE__') or define('__BASE__', 'http://ticket/');
-//defined('__BASE__') or define('__BASE__', 'http://lelian.ichurun.com/');
+//defined('__BASE__') or define('__BASE__', 'http://ticket/');
+defined('__BASE__') or define('__BASE__', 'http://ticket.ichurun.com/');
 // 定义应用目录
 define('APP_PATH','./Application/');
 
