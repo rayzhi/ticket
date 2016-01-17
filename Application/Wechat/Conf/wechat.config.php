580d421fb709ecbf3409b0525fe7870e5d1d75a0
dasheng523 <dasheng523@163.com>
2016-01-17 15:39:11 +0800

    修改微信配置

--- Application/Wechat/Conf/wechat.config.php
+++ Application/Wechat/Conf/wechat.config.php
@@ -6,7 +6,7 @@ return array(
 	'WECHAT_TOKEN' 			=> 'reraweraedfsdgdfgf1233211354344',
 	'WECHAT_APPID' 			=> 'wx55cfd637e5ee245b',
 	'WECHAT_APPSECRET' 	    => 'abe0c13c5209baf293b0ac64f5dd3331', 
-	'WECHAT_ENCODINGAESKEY' => 'bc5e446cd89da9d8eb5be44effe9d41e',
+	'WECHAT_ENCODINGAESKEY' => 'mwZcAgJFmqdjExG7Dny9oOaaFOsUTwdEaHQVOJ6SOJ6',
 	'WECHAT_WELCOME' 		=> '欢迎关注微信公众号！功能正在开发中，请耐心等待哦！期待你的加入哦~',
 	
 );
