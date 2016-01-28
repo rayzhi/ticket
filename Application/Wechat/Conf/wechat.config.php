166a3ff4ba25162a66cc3eda6a605e42c1a75a3e
dasheng523 <dasheng523@163.com>
2016-01-28 11:22:38 +0800

    各种修改

--- Application/Wechat/Conf/wechat.config.php
+++ Application/Wechat/Conf/wechat.config.php
@@ -23,7 +23,7 @@ return array(
             array('type'=>'view','name'=>'微信购票',
                 'sub_button'=>array(
                                 array('type'=>'view','name'=>'一键购票','url'=>__BASE__."index.php/Wechat/Ticket/tickeList"),
-                                array('type'=>'view','name'=>'推荐有礼','url'=>__BASE__."index.php/Wechat/Recommend/index"),
+                                array('type'=>'view','name'=>'分享有礼','url'=>__BASE__."index.php/Wechat/Recommend/index"),
                                 array('type'=>'view','name'=>'新春活动','url'=>__BASE__."index.php/Wechat/Activity/index?id=5"))),
             array('type'=>'view','name'=>'个人中心','url'=>__BASE__."index.php/Wechat/User/index"),
         )
